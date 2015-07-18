c59ffd4138f3603e8d530e8087b78e829ffa57d7
MJRafiei <rafiei1373@gmail.com>
2015-07-18 13:13:32 +0430

    home start

--- sepas_iran/urls.py
+++ sepas_iran/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'sepas_iran.views.home', name='home'),
+    url(r'^$', 'sepas_iran.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
---
+++ sepas_iran/views.py
@@ -0,0 +1,6 @@
+__author__ = 'Javad'
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+
+def home(request):
+    return render(request, 'home.html', {})
