b7de4642dc1d8f7c326591f6887f7c861b3baa83
armanMohammadi <armanmohammadi.am@gmail.com>
2015-07-21 09:17:04 +0430

    shekar khori ziyadi

--- sepas_iran/urls.py
+++ sepas_iran/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'sepas_iran.views.home', name='home'),
     url(r'^service/', include('service.urls')),
+    url(r'^finance/', include('management.urls')),
     url(r'^sale/', include('sale.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
