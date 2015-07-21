e3862453a0af0ca8cda7bf5f7a74b3b98cb5b504
armanMohammadi <armanmohammadi.am@gmail.com>
2015-07-21 11:38:03 +0430

    sale added! :D

--- management/urls.py
+++ management/urls.py
@@ -5,6 +5,8 @@ from django.views.generic.base import TemplateView
 __author__ = 'Iman'
 urlpatterns = [
     # Examples:
+
+    url(r'^finance$', TemplateView.as_view(template_name='management/financeReport.html'), name='service-list'),
     url(r'^advertisement$', TemplateView.as_view(template_name='management/advertisement.html'), name='service-list'),
     url(r'^service_activate$', TemplateView.as_view(template_name='management/service_activate.html'), name='service-list'),
     url(r'^$', TemplateView.as_view(template_name='management/dashboard.html'), name='service-list'),
--- sepas_iran/urls.py
+++ sepas_iran/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'sepas_iran.views.home', name='home'),
     url(r'^service/', include('service.urls')),
-    url(r'^finance/', include('management.urls')),
     url(r'^manage/', include('management.urls')),
 
     url(r'^sale/', include('sale.urls')),
