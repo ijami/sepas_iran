91bf47a2e27284a267c759998f112735990a2c0f
armanMohammadi <armanmohammadi.am@gmail.com>
2015-07-21 12:15:48 +0430

    majideshan

--- sale/urls.py
+++ sale/urls.py
@@ -7,4 +7,5 @@ __author__ = 'Iman'
 urlpatterns = [
     # Examples:
     url(r'^cart$', TemplateView.as_view(template_name='sale/cart.html'), name='cart'),
+    url(r'^shopping$', TemplateView.as_view(template_name='sale/sale.html'), name='shopping'),
 ]
