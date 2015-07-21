e04cccbfc1ebd7195e17261c19880b4d71716f75
MJRafiei <rafiei1373@gmail.com>
2015-07-21 07:11:20 +0430

    cart

---
+++ sale/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'Javad'
+
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+__author__ = 'Iman'
+urlpatterns = [
+    # Examples:
+    url(r'^cart$', TemplateView.as_view(template_name='sale/cart.html'), name='cart'),
+]
--- sepas_iran/urls.py
+++ sepas_iran/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'sepas_iran.views.home', name='home'),
     url(r'^service/', include('service.urls')),
+    url(r'^sale/', include('sale.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
