0484f2af1eca8f54e7b4f9f5079cff8243a65769
Ehsan <ehsannazarian1@gmail.com>
2015-07-22 00:00:00 +0430

    nearly completed

--- sepas_iran/views.py
+++ sepas_iran/views.py
@@ -1,6 +1,14 @@
 __author__ = 'Javad'
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
+from tourist.forms import MyUserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
+    if request.POST:
+
+        user = authenticate(username="ehsan", password="ehsan")
+        login(request, user)
+
     return render(request, 'home.html')
--- tourist/views/singup_view.py
+++ tourist/views/singup_view.py
@@ -1,6 +1,7 @@
 __author__ = 'Ehsan'
 from django.shortcuts import render, redirect
 from tourist.forms import MyUserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def register(request):
@@ -14,6 +15,10 @@ def register(request):
 
     if form.is_valid():
         form.save()
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        login(request, user)
         return redirect('/')
 
     return render(request, 'tourist/register.html', {
