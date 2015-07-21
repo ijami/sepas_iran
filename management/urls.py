9e56b3105595524c6ae70ba4a3ee38088a7d7cea
Iman <iman.jami@gmail.com>
2015-07-21 09:13:15 +0430

    dashboard added

--- management/urls.py
+++ management/urls.py
@@ -5,5 +5,7 @@ from django.views.generic.base import TemplateView
 __author__ = 'Iman'
 urlpatterns = [
     # Examples:
+    url(r'^advertisement$', TemplateView.as_view(template_name='management/advertisement.html'), name='service-list'),
+    url(r'^service_activate$', TemplateView.as_view(template_name='management/service_activate.html'), name='service-list'),
     url(r'^$', TemplateView.as_view(template_name='management/dashboard.html'), name='service-list'),
 ]
