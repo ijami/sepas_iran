f1d66bcfde4d7d3e721f9ed1a2d9e02c63fcf99e
Iman <iman.jami@gmail.com>
2015-07-21 08:06:37 +0430

    felan in ja

--- sepas_iran/urls.py
+++ sepas_iran/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'sepas_iran.views.home', name='home'),
     url(r'^service/', include('service.urls')),
+    url(r'^manage/', include('management.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
