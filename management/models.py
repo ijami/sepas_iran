42c19db337d63f2e71a05876fc7f1f2068707e1a
Iman <iman.jami@gmail.com>
2015-08-01 20:22:40 +0430

    added model to admin

--- management/models.py
+++ management/models.py
@@ -3,5 +3,8 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+from base.models import SiteUser
+
+
 class Manager(models.Model):
-    primary_user = models.ForeignKey(User)
+    primary_user = models.ForeignKey(SiteUser)
