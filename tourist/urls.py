d97243917324c3a199ff8bff4baf25745418a5b5
Ehsan <ehsannazarian1@gmail.com>
2015-08-01 14:21:10 +0430

    added login page. added container for base content block. some fixes

--- tourist/urls.py
+++ tourist/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Examples:
     url(r'^register/$', 'tourist.views.singup_view.register', name='register_tourist'),
     url(r'^forgot-password/$', 'tourist.views.forgot_pass_view.forgot_pass', name='forgot-password'),
+    url(r'^login/$', 'tourist.views.login_view.login_view', name='login_tourist'),
     url(r'^logout/$', 'tourist.views.login_view.logout_view', name='logout_tourist'),
     url(r'^financeReport/$', TemplateView.as_view(template_name='tourist/userFinanceReport.html'), name='user_finance_report')
 ]
--- tourist/views/login_view.py
+++ tourist/views/login_view.py
@@ -1,8 +1,13 @@
 __author__ = 'Iman'
 
 from django.contrib.auth import logout
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'tourist/login.html')
