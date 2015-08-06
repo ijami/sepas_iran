a300f0f42c676eeccb9cbb21f5a0d828f1c8339f
Ehsan <ehsannazarian1@gmail.com>
2015-08-06 16:46:34 +0430

    tourist register now working :)

--- base/views/login_view.py
+++ base/views/login_view.py
@@ -1,3 +1,4 @@
+from django.http.request import HttpRequest
 from base.forms import LoginForm
 
 __author__ = 'Iman'
@@ -6,8 +7,26 @@ from django.contrib.auth import logout, login, authenticate
 from django.shortcuts import redirect, render
 from django.core.urlresolvers import reverse
 
+import urllib.parse
+import urllib.request
+
 
 def logout_user(request):
+    # api_key = '724F4F72774A6F384751442F504F6F724447734D4B413D3D'
+    # url = 'http://api.kavenegar.com/v1/' + api_key + '/sms/send.json'
+    #
+    # values = {
+    #     'receptor': "09127929708",
+    #     'sender': '30006703323323',
+    #     'type': '1',
+    #     'message': "سلام. سامانه سپاس ایران به شما مرسی میگوید به خاطر اینکه هستید. ارسال شده از طریق کد. مسئول بخش تحقیق و توسعه :|"
+    # }
+    #
+    # data = urllib.parse.urlencode(values)
+    # binary_data = data.encode('utf-8')
+    # sms_req = urllib.request.Request(url, binary_data)
+    # sms_response = urllib.request.urlopen(sms_req)
+    # print(sms_response.read())
     logout(request)
     return redirect(reverse('home'))
 
