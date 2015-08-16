ed85b028135d451bb9179861bfd903353c215839
Ehsan <ehsannazarian1@gmail.com>
2015-08-16 04:56:04 +0430

    just for javad and morabbi :|

--- service_provider/static/service_provider/javascript/register_script.js
+++ service_provider/static/service_provider/javascript/register_script.js
@@ -2,8 +2,10 @@
  * Created by Ehsan on 8/15/2015.
  */
 $(document).ready(function(){
-    $('.ui.dropdown').dropdown({onChange:function(val){
-        $('.special-form[data-tab='+val+']').show();
-        $('.special-form[data-tab!='+val+']').hide();
-    }});
+    //$('.ui.dropdown').dropdown({onChange:function(val){
+    //    $('.special-form[data-tab='+val+']').show();
+    //    $('.special-form[data-tab!='+val+']').hide();
+    //}});
+    $('.ui.dropdown').dropdown();
+    $('.menu .item').tab();
 });
