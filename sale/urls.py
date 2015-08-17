903b276973451b36751a9429e650f09003fccb9f
MJRafiei <rafiei1373@gmail.com>
2015-08-18 01:41:15 +0430

    comment added

--- sale/urls.py
+++ sale/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # Examples:
     url(r'^cart$', 'sale.views.cart_view.cart_view', name='cart'),
     url(r'^service/([rtf]_[\d+]_[\d+])$', 'sale.views.service.service_show', name='service'),
+    url(r'comment$', 'sale.views.comment.add_comment', name='comment'),
 ]
--- sale/views/service.py
+++ sale/views/service.py
@@ -1,4 +1,6 @@
+import datetime
 from django.core.exceptions import ObjectDoesNotExist
+import jdatetime
 
 __author__ = 'MJR'
 from django.shortcuts import render, redirect
@@ -27,7 +29,15 @@ def service_show(request, sold_number):
     q = ServiceItem.objects.filter(service=service)
     size = len(q)
     remain = service.capacity - size
-    comments = service.comments
+    comments = service.comments.all()
+    cm = []
+    for c in comments:
+        print(c.send_time.date())
+        cm.append({
+            'text': c.text,
+            'sender': c.sender,
+            'send_time': jdatetime.date.fromgregorian(date=c.send_time.date())
+        })
     context = {
         'type': type,
         'image': service.image,
@@ -37,7 +47,7 @@ def service_show(request, sold_number):
         'remain': remain,
         'price': service.price,
         'service': service,
-        'comments': comments,
+        'comments': cm,
         'provider': provider,
     }
     return render(request, 'sale/service.html', context)
