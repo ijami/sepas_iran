6cd5d72cfba8d528f30c83366924e7b98a0e5703
armanMohammadi <armanmohammadi.am@gmail.com>
2015-08-19 21:41:25 +0430

    tamoom shod mali gooya

--- service_provider/urls.py
+++ service_provider/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 
 urlpatterns = [
     # Examples:
+    url(r'^report/$', 'service_provider.views.report_view.reprot_view', name='report_provider'),
     url(r'^panel/$', 'service_provider.views.panel_view.panel', name='service_provider_panel'),
     url(r'^panel/new_service$', 'service_provider.views.new_service.new_service', name='new_service'),
     url(r'^register/$', 'service_provider.views.register_view.register', name='register_service_provider'),
     url(r'^panel/service_list/$', 'service_provider.views.service_list.service_list', name='provider_service_list'),
     url(r'^panel/service_list/capa', 'service_provider.views.service_list.add_capacity', name='add_service_capacity'),
     url(r'^panel/ads$', 'service_provider.views.ad_request.advertisement_request', name='ads'),
-    url(r'information/(\d+)$', 'service_provider.views.information.information', name='sag')
+    url(r'^information/(\d+)$', 'service_provider.views.information.information', name='sag')
 ]
