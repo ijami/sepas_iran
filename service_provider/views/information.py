168abe0f22bd950acd03ec4e851e435f864b7240
MJRafiei <rafiei1373@gmail.com>
2015-08-20 02:28:15 +0430

    sale completed

--- service_provider/views/information.py
+++ service_provider/views/information.py
@@ -5,7 +5,7 @@ __author__ = 'MJR'
 
 
 def information(request, id):
-    user = SiteUser.objects.get(username=id)
+    user = SiteUser.objects.get(id=id)
     context = user.get_fields()
     data = {'context': context}
     return render(request, 'service_provider/provider_information.html', data)
