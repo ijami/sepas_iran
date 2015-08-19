6e22358350a8fba7ae6dc806a86dc3456638b2e9
po0ya <pooya.mosaddegh72@gmail.com>
2015-08-19 09:47:49 +0000

    set

--- management/admin.py
+++ management/admin.py
@@ -5,17 +5,4 @@ from management.models import AdvertiseBox
 from sale.models import Cart, Factor, ServiceItem
 
 
-class ServiceInline(admin.TabularInline):
-    model = ServiceItem
-
-
-class CartAdmin(admin.ModelAdmin):
-    inlines = [ServiceInline]
-
-
-class FactorAdmin(admin.ModelAdmin):
-    inlines = [ServiceInline]
-
-admin.site.register(Cart, CartAdmin)
-admin.site.register(Factor)
 admin.site.register(AdvertiseBox)
