c2cce6af3d07b502728e0fd0ff3fc952a3958405
armanMohammadi <armanmohammadi.am@gmail.com>
2015-08-20 02:30:18 +0430

    temaaammmm

--- tourist/views/task_recommended_email.py
+++ tourist/views/task_recommended_email.py
@@ -4,15 +4,23 @@ from django.contrib.auth.models import User
 from base.views.send_mail import send_mail
 from tourist.views.crm_function import send_recommended_mail
 from tourist.models import Tourist
+import jdatetime
 # from jdatetime.jalali import GregorianToJalali
 class MyCronJob(CronJobBase):
-    RUN_EVERY_MINS = 1 # every 2 hours
+    RUN_EVERY_MINS = 1  # every 2 hours
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'tourist.views.task_recommended_email' # a unique code
+    code = 'tourist.views.task_recommended_email'  # a unique code
+
     def do(self):
         tourists = Tourist.objects.all()
         for tourist in tourists:
             recommendations = send_recommended_mail(tourist.primary_user.id)
             recommendations.sort(key=lambda x: x.sold_number)
-            send_mail('پیشنهادات سپاس ایران', 'b.i.sepasiran@gmail.com', [tourist.primary_user.email], 'tourist/mail_birthday.txt',
-                      'tourist/recommendation.html', {'recommendations': recommendations,'tourist':tourist}, True)
+            times = {}
+            for i, recommendation in enumerate(recommendations):
+                times.update(
+                    {i: jdatetime.date.fromgregorian(date=recommendation.get_date()).strftime("%Y/%m/%d")})
+            send_mail('پیشنهادات سپاس ایران', 'b.i.sepasiran@gmail.com', [tourist.primary_user.email],
+                      'tourist/mail_birthday.txt',
+                      'tourist/recommendation.html',
+                      {'recommendations': recommendations, 'tourist': tourist, 'times': times}, True)
